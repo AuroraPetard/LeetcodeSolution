7eab51b68354f7420c6bf6706d204d138bb994ba
lzq <lzq@mbp.local>
2022-09-23 17:44:34 +0800

    idea

--- src/main/java/com/AuroraPetard/MAP_3/Solution1.java
+++ src/main/java/com/AuroraPetard/MAP_3/Solution1.java
@@ -7,53 +7,55 @@ import java.util.Map;
 
 class Solution1 {
     // https://leetcode.cn/problems/find-all-anagrams-in-a-string/
+    public static List<Integer> findAnagrams(String s, String p) {
+        Map<String, List<Integer>> map = new HashMap<>();
 
+        for (int i = 0; i <= s.length() - p.length(); i++) {
+            String sub = s.substring(i, i + p.length());
+            int[] count = new int[26];
 
-    public List<Integer> findAnagrams(String s, String p) {
-        Map<String,List<Integer>> map=new HashMap<>();
-
-        for(int i=0; i< s.length()-p.length();i++){
-            String sub=s.substring(i,i+p.length());
-            int[] count=new int[26];
-
-            for(int j=0;j<sub.length();j++){
-                count[sub.charAt(j)-'a']++;
+            for (int j = 0; j < sub.length(); j++) {
+                count[sub.charAt(j) - 'a']++;
             }
 
-            StringBuffer sb=new StringBuffer();
+            StringBuffer sb = new StringBuffer();
 
-            for(int k=0;k<26;k++){
-                if(count[k]!=0){
-                    sb.append((char)('a'+k));
+            for (int k = 0; k < 26; k++) {
+                if (count[k] != 0) {
+                    sb.append((char) ('a' + k));
                     sb.append(count[k]);
                 }
             }
-            String key=sb.toString();
+            String key = sb.toString();
 
-            List<Integer> list=map.getOrDefault(key,new ArrayList<Integer>());
+            List<Integer> list = map.getOrDefault(key, new ArrayList<Integer>());
 
             list.add(i);
 
-            map.put(key,list);
+            map.put(key, list);
         }
 
-        int[] count=new int[26];
+        int[] count = new int[26];
 
-        for(int j=0;j<p.length();j++){
-            count[p.charAt(j)-'a']++;
+        for (int j = 0; j < p.length(); j++) {
+            count[p.charAt(j) - 'a']++;
         }
 
-        StringBuffer sb=new StringBuffer();
+        StringBuffer sb = new StringBuffer();
 
-        for(int k=0;k<26;k++){
-            if(count[k]!=0){
-                sb.append((char)('a'+k));
+        for (int k = 0; k < 26; k++) {
+            if (count[k] != 0) {
+                sb.append((char) ('a' + k));
                 sb.append(count[k]);
             }
         }
-        String key=sb.toString();
+        String key = sb.toString();
 
-        return map.get(key);
+        return map.getOrDefault(key,new ArrayList<Integer>());
 
+    }
+
+    public static void main(String[] args) {
+        findAnagrams("abab","ab");
     }
 }
